97af177ca67c3ff5928a08c73d2e42d6102e423f
Soumyadeep Chatterjee <soumyadeepchatterjee803@gmail.com>
2025-01-18 20:06:17 -0500

    first commit

---
+++ Moonshot/Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Soumyadeep Chatterjee on 1/18/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
