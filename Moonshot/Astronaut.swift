eaa02a02f3729d7f641de94e0597d426c2033632
Soumyadeep Chatterjee <soumyadeepchatterjee803@gmail.com>
2025-02-28 23:26:00 -0500

    initial

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Soumyadeep Chatterjee on 2/28/25.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
